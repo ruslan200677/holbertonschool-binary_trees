5d0e0cbc431dae161371e9c20feb2e8512856cb3
ruslan200677 <qaqli20033@gmail.com>
2024-08-07 12:41:13 +0400

    1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,7 +10,7 @@
  * Return: the new
  */
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
