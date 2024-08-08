0484524b2105f4d2a0a215c80ba8fcf35f0bd6b1
Sabillva <itifstudentsabina@gmail.com>
2024-08-08 13:05:58 +0000

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!treeu->left && !tree->right)
+	if (!tree->left && !tree->right)
 		return (1);
 
 	count += binary_tree_leaves(tree->left);
